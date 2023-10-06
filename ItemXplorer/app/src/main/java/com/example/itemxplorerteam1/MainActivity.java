d1b26c152f8819c1420ac3121d3b91260b7294ff
yogesh-naidu <yogeshnaidu33@gmail.com>
2023-10-06 18:12:41 -0500

    Added Main Page and Navigated it to Login Page (Yogesh Naidu Mahareddy)

---
+++ ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.itemxplorerteam1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button loginButton = findViewById(R.id.loginBTN);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an Intent to navigate to the LoginActivity
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                // Start the LoginActivity
+                startActivity(intent);
+            }
+        });
+    }
+}
