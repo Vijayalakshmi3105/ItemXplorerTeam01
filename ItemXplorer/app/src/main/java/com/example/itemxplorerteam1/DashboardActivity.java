06a2974981888d08eb8309b41fb85d078a180fa6
sumanthreddy76 <sumanthreddynandyala@gmail.com>
2023-10-06 21:59:00 -0500

    Added Delete Items Page and navigated it from Dashboard Activity (Sumanth Reddy Nandyala)

--- ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/DashboardActivity.java
+++ ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/DashboardActivity.java
@@ -2,7 +2,10 @@ package com.example.itemxplorerteam1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Button button = findViewById(R.id.removeItemsBTN);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an Intent to navigate to the RemoveItemsActivity
+                Intent intent = new Intent(DashboardActivity.this, RemoveItemsActivity.class);
+
+                // Start the RemoveItemsActivity
+                startActivity(intent);
+            }
+        });
     }
 }
