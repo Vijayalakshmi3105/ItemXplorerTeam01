9af0ecf0f6eb2e3c1e7fd941c41170747a60e4f2
sumanthreddy76 <sumanthreddynandyala@gmail.com>
2023-10-06 19:07:04 -0500

    Added some features to Login page

--- ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/MainActivity.java
+++ ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.itemxplorerteam1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginButton = findViewById(R.id.loginBTN);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an Intent to navigate to the LoginActivity
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                // Start the LoginActivity
+                startActivity(intent);
+            }
+        });
     }
 }
