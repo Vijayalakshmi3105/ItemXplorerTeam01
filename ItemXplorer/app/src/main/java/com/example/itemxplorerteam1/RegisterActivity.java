37baaba5ad08ef1d4f203f2bbb99059a74848b56
Vijayalakshmi Sankati <vijayareddy1998@gmail.com>
2023-10-27 19:55:53 -0500

    UI updated and functionality added for Register and Main Activities (Vijayalakshmi Sankati)

--- ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/RegisterActivity.java
+++ ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/RegisterActivity.java
@@ -1,30 +1,154 @@
 package com.example.itemxplorerteam1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.*;
+//import com.google.firebase.
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextName, editTextEmail, editTextPassword, editTextPhone,editTextcPassword;
+    public Button UserRegisterBtn;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        Button registerButton = findViewById(R.id.signupBTN);
+        editTextName = findViewById(R.id.departmentName);
+        editTextEmail = findViewById(R.id.emailRegister);
+        editTextPassword = findViewById(R.id.passwordRegister);
+        editTextcPassword= findViewById(R.id.confirmPassword);
+        UserRegisterBtn= findViewById(R.id.button_register);
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
 
-        registerButton.setOnClickListener(new View.OnClickListener() {
+        mAuth = FirebaseAuth.getInstance();
+
+
+        UserRegisterBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                // Create an Intent to navigate to the LoginActivity
-                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-
-                // Start the LoginActivity
-                startActivity(intent);
+            public void onClick(View v) {
+                registerUser();
             }
         });
+
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() != null) {
+            //handle the already login user
+        }
+    }
+
+
+    private void registerUser() {
+        final String name = editTextName.getText().toString().trim();
+        final String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString().trim();
+        String cpassword = editTextcPassword.getText().toString().trim();
+        // final String phone = editTextPhone.getText().toString().trim();
+        if (email.isEmpty()) {
+            editTextEmail.setError("It's empty");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (name.isEmpty()) {
+            editTextName.setError("It's Empty");
+            editTextName.requestFocus();
+            return;
+        }
+
+
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Not a valid emailaddress");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Its empty");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Less length");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(!password.equals(cpassword)){
+            editTextcPassword.setError("Password Donot Match");
+            editTextcPassword.requestFocus();
+            return;
+        }
+
+
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+
+                            final User user = new User(
+                                    name,
+                                    email
+
+                            );
+                            FirebaseUser usernameinfirebase = mAuth.getCurrentUser();
+                            String UserID=usernameinfirebase.getEmail();
+                            String resultemail = UserID.replace(".","");
+//                            https://console.firebase.google.com/project/itemxplorer/authentication/users
+//                            FirebaseDatabase.getInstance("https://inventory-management-fd662-default-rtdb.firebaseio.com").getReference("Users")
+                            FirebaseDatabase.getInstance("https://console.firebase.google.com/project/itemxplorer/authentication/users").getReference("Users")
+                                    .child(resultemail).child("UserDetails")
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            progressBar.setVisibility(View.GONE);
+                                            Toast.makeText(RegisterActivity.this, "Registration Success", Toast.LENGTH_LONG).show();
+                                            startActivity(new Intent(RegisterActivity.this,dashboardActivity.class));
+                                        }
+                                    });
+
+                        } else {
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(RegisterActivity.this, "Registration Failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+
+    }
+
+
+
 }
