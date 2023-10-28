9af98c1b2d807e400371767eb92f01e486a3dcfa
Vijayalakshmi Sankati <vijayareddy1998@gmail.com>
2023-10-27 19:51:25 -0500

    Adding new chanages

--- ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/MainActivity.java
+++ ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/MainActivity.java
@@ -1,40 +1,40 @@
 package com.example.itemxplorerteam1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button loginButton = findViewById(R.id.loginBTN);
-        Button RegisterButton = findViewById(R.id.RegisterBTN);
+        auth = FirebaseAuth.getInstance();
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create an Intent to navigate to the LoginActivity
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        FirebaseUser user = auth.getCurrentUser();
 
-                // Start the LoginActivity
-                startActivity(intent);
-            }
-        });
-        RegisterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create an Intent to navigate to the RegisterActivity
-                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        if(user != null){
+            finish();
+            startActivity(new Intent(this, dashboardActivity.class));
+        }
 
-                // Start the RegisterActivity
-                startActivity(intent);
-            }
-        });
+    }
+
+
+    public void login (View view)
+    {
+
+        startActivity(new Intent(this,LoginActivity.class));
+    }
+    public void register (View view)
+    {
+        startActivity(new Intent(this,RegisterActivity.class));
     }
 }
+
