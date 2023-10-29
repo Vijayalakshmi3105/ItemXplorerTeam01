c6963535a0da01fa0e6d504108c76aedca57a3b2
Vijayalakshmi Sankati <vijayareddy1998@gmail.com>
2023-10-28 21:24:57 -0500

    Added activity_add_item and UI and connected to MainActivity (Sumanth Reddy Nandyala)

--- ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/AddItemActivity.java
+++ ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/AddItemActivity.java
@@ -8,35 +8,35 @@ import android.view.View;
 import android.widget.Button;
 
 public class AddItemActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_item);
-
-        Button buttons = findViewById(R.id.addItembutton);
-        Button qrButton = findViewById(R.id.qrButton);
-
-        buttons.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create an Intent to navigate to the RemoveItemsActivity
-                Intent intent = new Intent(AddItemActivity.this, ViewItemActivity.class);
-
-                // Start the RemoveItemsActivity
-                startActivity(intent);
-            }
-        });
-
-        qrButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create an Intent to navigate to the LoginActivity
-                Intent intent = new Intent(AddItemActivity.this, BarcodeActivity.class);
-
-                // Start the LoginActivity
-                startActivity(intent);
-            }
-        });
-    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_add_item);
+//
+//        Button buttons = findViewById(R.id.addItembutton);
+//        Button qrButton = findViewById(R.id.qrButton);
+//
+//        buttons.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Create an Intent to navigate to the RemoveItemsActivity
+//                Intent intent = new Intent(AddItemActivity.this, ViewItemActivity.class);
+//
+//                // Start the RemoveItemsActivity
+//                startActivity(intent);
+//            }
+//        });
+//
+//        qrButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Create an Intent to navigate to the LoginActivity
+//                Intent intent = new Intent(AddItemActivity.this, BarcodeActivity.class);
+//
+//                // Start the LoginActivity
+//                startActivity(intent);
+//            }
+//        });
+//    }
 }
