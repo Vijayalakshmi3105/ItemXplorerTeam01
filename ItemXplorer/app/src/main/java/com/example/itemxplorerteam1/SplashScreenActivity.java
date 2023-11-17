bd29fc6c32a2d07b22ff49d401e19168cb62b0e8
ChundruNeelima <123058424+ChundruNeelima@users.noreply.github.com>
2023-11-16 18:44:17 -0600

    Added SearchItemsActivity, SplashScreenActivty java and xml files too(Neelima , Chundru)

--- ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/SplashScreenActivity.java
+++ ItemXplorer/app/src/main/java/com/example/itemxplorerteam1/SplashScreenActivity.java
@@ -1,4 +1,32 @@
 package com.example.itemxplorerteam1;
 
-public class SplashScreenActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashTimeOut=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_splash);
+        logo=(ImageView)findViewById(R.id.logo);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+    }
 }
